b9b77a4df8e9413724ab649020b34dbff3cae2f7
Joshua Eze <ezejoshuac@gmail.com>
2022-11-17 10:33:38 +0100

    Add project files.

---
+++ Pomdro/Pomodoro.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pomdro
+{
+    internal class Pomdro
+    {
+        public int workTimer;
+        public int restTimer;
+
+        public void play()
+        {
+            Console.WriteLine("Please set work duration: ");
+            var workInputTimer = Console.ReadLine();
+            Console.WriteLine("Please set rest duration");
+            var restInputTimer = Console.ReadLine();
+            try
+            {
+                workTimer = Convert.ToInt32(workInputTimer);
+                restTimer = Convert.ToInt32(restInputTimer);
+                startWork();
+                startRest();
+            }
+            catch
+            {
+                Console.WriteLine("wrong entry");
+                play();
+            }
+        }
+        
+        public void startWork()
+        {
+            var workTime = new DateTime(2000, 1, 1, 0, workTimer, 0);
+
+            for (int i = 0; i < workTimer * 60; i++)
+            {
+                Console.Write("Work Time Remains : {0}", workTime.ToString("mm:ss"));
+                workTime = workTime.AddSeconds(-1);
+                Thread.Sleep(1000);
+                Console.Clear();
+            }
+        }
+
+        public void startRest()
+        {
+            var restTime = new DateTime(2000, 1, 1, 0, restTimer, 0);
+            for (int i = 0; i < workTimer * 60; i++)
+            {
+                Console.Write($"Rest Time Remains : {0}", restTime.ToString("mm:ss"));
+                restTime = restTime.AddSeconds(-1);
+                Thread.Sleep(1000);
+                Console.Clear();
+            }
+
+        }
+    }
+}
---
+++ Pomdro/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Pomdro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Sleep for 2 seconds.");
+                Thread.Sleep(3000);
+            }*/
+            Pomdro pomdro = new Pomdro();
+            pomdro.play();
+        }
+    }
+}
